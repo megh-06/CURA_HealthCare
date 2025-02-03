8726b40b3aff88c99a94e55b52152baaa12df8ef
Meghraj Yadav <ymeghraj18@gmail.com>
2025-02-03 18:22:50 +0530

    Cross Browser Code added

--- CURA_HealthCare/Utilities/Base.cs
+++ CURA_HealthCare/Utilities/Base.cs
@@ -1,10 +1,14 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
 
 namespace CURA_HealthCare.Utilities
 {
@@ -15,9 +19,32 @@ namespace CURA_HealthCare.Utilities
         [SetUp]
         public void Setup()
         {
-            driver = new ChromeDriver();
+            //driver = new ChromeDriver();
+
+            string browserName = ConfigurationManager.AppSettings["browser"] ?? "Chrome";
+            InitBrowser(browserName);
+
             driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Url = "https://katalon-demo-cura.herokuapp.com/";
+        }
+
+        public void InitBrowser(string browserName)
+        {
+            switch (browserName.ToLower())
+            {
+                case "edge":
+                    new DriverManager().SetUpDriver(new EdgeConfig());
+                    break;
+
+                case "chrome":
+                    new DriverManager().SetUpDriver(new ChromeConfig());
+                    driver = new ChromeDriver();
+                    break;
+
+                default:
+                    throw new ArgumentException($"Invalid browser: {browserName}");
+            }
         }
 
         [TearDown]
